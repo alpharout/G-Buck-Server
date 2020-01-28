6b5b17e96525e02f2603e1f20fc6148ec5f038ca
Ge <55292128+alpharout@users.noreply.github.com>
2020-01-28 15:58:14 +0100

    v0.0.0 [4]

--- src/de/craftrout/gbuck/server/api/Server.java
+++ src/de/craftrout/gbuck/server/api/Server.java
@@ -1,11 +1,13 @@
 package de.craftrout.gbuck.server.api;
 
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
     private ServerSocket serverSocket;
+    private DataOutputStream dataOutputStream;
 
     public Server(int port) throws IOException {
         serverSocket = new ServerSocket(port);
@@ -13,6 +15,9 @@ public class Server {
 
     public boolean waitForClient() throws IOException {
         Socket client = serverSocket.accept();
+        dataOutputStream = new DataOutputStream(client.getOutputStream());
+
+        dataOutputStream.writeUTF("Hallo Welt!");
 
         return true;
     }
